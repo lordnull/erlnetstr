9461b1bf1b0e19553332d80c0ef7fe5d75ea2049
Micah Warren <micahw@lordnull.com>
2011-11-29 14:11:40 -0600

    Added tests, now to fix code it's testing.

--- test/netstring_tests.erl
+++ test/netstring_tests.erl
@@ -1,3 +1,30 @@
 -module(netstring_tests).
 -include_lib("eunit/include/eunit.hrl").
 
+encode_test_() -> [
+	?_assertEqual(<<"5:hello,">>,
+		netstring:encode(<<"hello">>)),
+
+	?_assertEqual(<<"5:hello,5:world,">>,
+		netstring:encode([<<"hello">>, <<"world">>])),
+
+	?_assertEqual(<<"B:hello world,">>,
+		netstring:encode(<<"hello world">>, 16)),
+
+	?_assertEqual(<<"B:hello world,1:!,">>,
+		netstring:encode([<<"hello world">>, <<$!>>], 16))
+].
+
+decode_test_() -> [
+	?_assertMatch({[<<"hello">>], _},
+		netstring:decode(<<"5:hello,">>)),
+
+	?_assertMatch({[<<"hello">>, <<"world">>], _},
+		netstring:decode(<<"5:hello,5:world,">>)),
+
+	?_assertMatch({[<<"hello world">>], _},
+		netstring:decode(<<"B:hello world">>, 16)),
+
+	?_assertMatch({[<<"hello world">>, <<$!>>], _},
+		netstring:decode(<<"B:hello world,1:!,">>, 16))
+].
